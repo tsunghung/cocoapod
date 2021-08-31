d2597049d75f108de0c04a686fc170c19b2fb605
Andy <tsunghung@google.com>
2021-08-31 16:35:16 -0700

    -ObjC

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
         .linkedFramework("JavaScriptCore"),
         .linkedFramework("SystemConfiguration"),
         .linkedFramework("UIKit"),
-        .unsafeFlags("-ObjC"),
+        .unsafeFlags(["-ObjC"]),
       ]
     ),
     .binaryTarget(
